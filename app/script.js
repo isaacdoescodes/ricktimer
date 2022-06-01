91c4bc176b46740bc45957738fef5037384915e5
Isaac Boorman <iceboorman@gmail.com>
2022-06-01 11:11:09 +0100

    better popup support

--- app/script.js
+++ app/script.js
@@ -2,10 +2,10 @@ function starttimer(secs) {
     if (secs == "") {
         secs = 60;
     }
-    var lol = confirm("Click 'OK' if you have enabled popup windows. Press 'Cancel' if you haven't and then go do so.");
     document.title = "New Tab";
     document.getElementById("manual").style.display = "none";
     window.setTimeout(() => {
-        window.open("https://www.youtube.com/watch?v=a3Z7zEc7AXQ");
+        window.open("https://www.youtube.com/watch?v=a3Z7zEc7AXQ")
     }, (secs - .5) * 1000)
 }
+localStorage.worked = true;
